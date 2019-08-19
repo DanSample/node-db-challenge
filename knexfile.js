59714d6066f397a6e5b234efdcddb836b8c35f30
Dan Sample <danssample@gmail.com>
2019-08-18 21:59:11 -0700

    built out seed data for db

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+// Update with 'your config settings.
 
 module.exports = {
   development: {
@@ -9,6 +9,9 @@ module.exports = {
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations/'
+    },
+    seeds: {
+      directory: './database/seeds/'
     }
   }
 };
